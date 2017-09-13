7d27e15f5d3d8fd4999346ec7470dee0dd8c6768
ben <blueavecado21@gmail.com>
2017-09-13 18:07:19 +0100

    blue avo commit

---
+++ my-page/index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="skin/bower_components/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="skin/css/styles.css">
+	<link rel="stylesheet" type="text/css" href="skin/bootstrap/jasny-bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="skin/css/stars.css">
+	<title>Blue Avacado | Web Design</title>
+</head>
+<html>
+
+<div id='stars'></div>
+<div id='stars2'></div>
+<div id='stars3'></div>
+
+<body class="blue-avacado-body">
+	<section class="container-fluid blue-avacado-top">
+		<div class="row wrapper">
+			<div class="col-sm-8 block-left">
+				<img src="" alt="logo" />
+			</div>
+
+			<div class="col-sm-4 block-right">
+				<a href="#" class="btn primary-btn">contact</a>
+			</div>
+		</div>
+	</section>
+
+	<section class="container-fluid blue-avacado-banner">
+		<div class="row wrapper">
+			<div class="col-sm-6 block-left">
+			<img src="images/part-one.png" class="img-responsive" alt="web-banner" />
+			</div>
+
+			<div class="col-sm-6 block-right">
+				<h1>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h1>
+				<h2>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h2>
+				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+			</div>
+		</div>
+	</section>
+
+	<div class="section-bg">
+		<section class="container-fluid blue-avacado-technologies-header">
+			<div class="row wrapper">
+				<div class="col-sm-12 block">
+					<h2>Here are some of the technologies I use!</h2>
+					<span class="line"></span>
+				</div>
+			</div>
+		</section>
+
+		<section class="container-fluid blue-avacado-technologies-body">
+			<div class="row wrapper">
+				<div class="col-sm-4 block">
+					<img src="images/html.png" class="img-responsive" />
+				</div>
+				<div class="col-sm-4 block">
+					<img src="images/git.png" class="img-responsive git" />
+				</div>
+				<div class="col-sm-4 block">
+					<img src="images/gulp-bower.png" class="img-responsive gulp" />
+				</div>
+			</div>
+		</section>
+	</div>
+
+	<section class="container-fluid blue-avacado-about-me">
+		<div class="row wrapper">
+			<div class="col-sm-6 block-left">
+			<h2>A tad about me! </h2>
+			<p>> Lorem Ipsum is simply dummy text of the printing.<br />
+			   > Lorem Ipsum is simply dummy text of the printing.<br />
+			   > Lorem Ipsum is simply dummy text of the printing.<br />
+			   > Lorem Ipsum is simply dummy text of the printing.<br />
+			   > Lorem Ipsum is simply dummy text of the printing.<br />		
+			   > Lorem Ipsum is simply dummy text of the printing.<br />
+			   > Lorem Ipsum is simply dummy text of the printing.</p>
+			</div>
+			<div class="col-sm-6 block-right">
+				<h2>User Friendly</h2>
+				<img src="images/thumbsup.png" class="img-responsive">
+			</div>
+		</div>
+	<section>	
+</body>
+
+<footer>
+<h1>This is a footer</h1>
+</footer>
+
+</html>
+
+<script type="text/javascript" src="skin/bower_components/jquery/dist/jquery.js"></script>
+<script type="text/javascript" src="skin/bower_components/bootstrap-sass/assets/javascripts/bootstrap.js"></script>
+<script type="text/javascript" src="skin/bootstrap/jasny-bootstrap.js"></script>
+<script type="text/javascript" src="skin/js/particles/particles.js"></script>
+<script type="text/javascript" src="skin/js/particles/app.js"></script>
